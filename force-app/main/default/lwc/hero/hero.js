710c2941227709aaac6fef45c67a31e7fe59d9fe
Govind Singh Solanki <gssolanki30@gmail.com>
2024-06-17 23:23:51 +0530

    Added a LWC component and static resources for Hero section.

---
+++ force-app/main/default/lwc/hero/hero.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/PortfolioImages'
+
+export default class Hero extends LightningElement {
+
+    //Assigning imported hero section images
+    heroImage = IMAGES + '/hero.jpg';
+}
