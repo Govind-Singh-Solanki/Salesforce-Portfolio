741969d3666d222aa3d9fecdd3b38ed9e00d8a7a
Govind Singh Solanki <gssolanki30@gmail.com>
2024-10-17 11:49:29 +0530

    Contact Me Button finished.

--- force-app/main/default/lwc/contactMeButton/contactMeButton.js
+++ force-app/main/default/lwc/contactMeButton/contactMeButton.js
@@ -1,11 +1,23 @@
 import { api, LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import createLead from '@salesforce/apex/ContactMeController.createLead';
 
 export default class ContactMeButton extends LightningElement {
     
     dialog;
+    fname;
+    lname;
+    company;
+    email;
+    description;
 
     renderedCallback(){
         this.dialog = this.template.querySelector('.contact-dialog');
+        this.fname = this.template.querySelector('.fname');
+        this.lname = this.template.querySelector('.lname');
+        this.company = this.template.querySelector('.company');
+        this.email = this.template.querySelector('.email');
+        this.description = this.template.querySelector('.description');
     }
 
     showDialog(event){
@@ -14,5 +26,44 @@ export default class ContactMeButton extends LightningElement {
 
     closeDialog(event){
         this.dialog.close();
+    }
+
+    handleSubmit(event){
+        const fnameValue = this.fname.value;
+        const lnameValue = this.lname.value;
+        const companyValue = this.company.value;
+        const emailValue = this.email.value;
+        const descriptionValue = this.description.value;
+        
+        if (lnameValue === ""){
+            this.showToast('Missing Required Field!', 'Last Name is a required field', 'error');
+            return;
+        } else if (emailValue === ""){
+            this.showToast('Missing Required Field!', 'Email is a required field', 'error');
+            return;
+        } else if (descriptionValue === ""){
+            this.showToast('Missing Required Field!', 'Description is a required field', 'error');
+            return;
+        } else {
+            createLead({
+                fname: fnameValue,
+                lname: lnameValue,
+                company: companyValue,
+                email: emailValue,
+                description: descriptionValue
+            });
+
+            this.closeDialog();    //for closing the dialog box
+            this.showToast('Submitted!', 'Your request has been submitted.', 'success');
+        }
+    }
+    
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(event);
     }
 }
