7e8f80cafa5a032e222878492c0cfdf4623a123f
Govind Singh Solanki <gssolanki30@gmail.com>
2024-10-20 13:41:51 +0530

    Added about me section to home page.

---
+++ force-app/main/default/lwc/aboutMe/aboutMe.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/PortfolioImages'
+
+export default class AboutMe extends LightningElement {
+
+    aboutMeImage = IMAGES + '/about-me.jpg'
+
+}
